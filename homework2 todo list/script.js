36466dfdf0209c3ad23765b5e37ceb88d7dbf72c
Bartosz Kloc <atlf75800@gmail.com>
2023-04-22 16:07:47 +0200

    homework2: can add new element to the list

--- homework2 todo list/script.js
+++ homework2 todo list/script.js
@@ -1,3 +1,12 @@
 function onAdd() {
-    alert("bangla");
+    const inputValue = document.getElementById("input").value;
+    const element = document.createElement("li");
+    const textNode = document.createTextNode(inputValue);
+    element.appendChild(textNode);
+  if (inputValue === '') {
+    alert("Input field is empty");
+  } else {
+    document.getElementById("list").appendChild(element);
+  }
+  document.getElementById("input").value = "";
 }
