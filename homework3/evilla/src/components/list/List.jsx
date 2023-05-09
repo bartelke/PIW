36c3155af34228cac9a5114ccdf42d24b680fce6
Bartosz Kloc <atlf75800@gmail.com>
2023-05-09 11:02:40 +0200

    homework3: List.css

--- homework3/evilla/src/components/list/List.jsx
+++ homework3/evilla/src/components/list/List.jsx
@@ -1,21 +1,22 @@
 import React, { useState } from 'react';
 import Add from '../add/Add';
+import '../list/List.css';
 
 function List() {
   const [list, setList] = useState([
     {
-      name: "House 1",
-      description: "lorem ipsum",
-      bedrooms: 2,
-      price: 450000,
-      address: "ul. Warszawska 1, Warszawa"
+      name: "Villa C13",
+      description: "villa with round windows, in summer you will feel the tropical atmosphere of the place",
+      bedrooms: 12,
+      price: 8000000,
+      address: "wybrzeże Stanisława Wyspiańskiego 23-25, 50-370 Wrocław"
     },
     {
-      name: "House 2",
-      description: "Duży dom z ogrodem i garażem",
+      name: "Villa C16",
+      description: "a large estate with modern architecture located in a place known for the conquests of the Vikings, you can still meet their descendants here",
       bedrooms: 4,
-      price: 850000,
-      address: "ul. Kościuszki 1, Kraków"
+      price: 6000000,
+      address: "Janiszewskiego 7, 50-372 Wrocław"
     }
   ]);
   
@@ -36,19 +37,24 @@ function List() {
   };
 
   return (
-    <div>
+    <div class="container">
+      <div class="header">
+        <h1>Book house to find your dream place!</h1>
         <button onClick={toggleAdd}>{showAdd ? 'Cancel' : 'Add New'}</button>
-        {showAdd && (<Add handleAddItem={handleAddItem} />)}
-      <h1>Evilla offers:</h1>
+          {showAdd && (<Add handleAddItem={handleAddItem} />)}
+      </div>
+        
       <ul>
         {list.map((item, index) => (
           <li key={index}>
             <h2>{item.name}</h2>
             <p>{item.description}</p>
             <p>{item.bedrooms} bedrooms</p>
-            <p>Price: {item.price} pln</p>
             <p>Adress: {item.address}</p>
-            <button onClick={() => handleDeleteItem(index)}>Delete</button>
+            <div class="footer">
+              <p><strong>Price: {item.price} pln</strong></p>
+              <button onClick={() => handleDeleteItem(index)}>Delete</button>
+            </div>
           </li>
         ))}
       </ul>
