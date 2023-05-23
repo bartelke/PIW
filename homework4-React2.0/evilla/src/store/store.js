57e34f7fd9d1fdad55e746d0df08333ee639b65d
Bartosz Kloc <atlf75800@gmail.com>
2023-05-23 19:02:19 +0200

    homework4 (tag lab3) finished

---
+++ homework4-React2.0/evilla/src/store/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit'
+import offersReducer from "../features/offers/offersSlice"
+
+export default configureStore({
+  reducer: {offers:offersReducer},
+})
